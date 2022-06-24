75f5ce911213c9802d8382751b1d31db8a034b63
Aman Harsh <amanharsh0399@gmail.com>
2022-06-24 14:26:17 +0530

    update: adding source code

---
+++ index.js
@@ -0,0 +1,66 @@
+var readLine = require("readline-sync")
+
+var score = 0
+
+var userName = readLine.question("Please Enter Your Name: ")
+
+console.log("\nHey! " + userName + ". Welcome to DO YOU KNOW Aman")
+
+var userResponse = readLine.question("\nLet's Start... Press enter to continue: ")
+
+console.log()
+
+function play(question, answer, rightAnswer){
+  var userAnswer = readLine.question(question)
+
+  if(userAnswer.toUpperCase() === answer.toUpperCase()){
+    console.log("Yay! You're Right.")
+    score += 1
+  }
+  else{
+    console.log("Oops! You're Wrong.")
+    console.log(rightAnswer)
+  }
+
+  console.log("Current score: " + score)
+  console.log("----------------")
+}
+
+var questionOne = {
+  question: "Am I older than 22? ",
+  answer: "yes",
+  rightAnswer: "I'm 23!"
+}
+
+var questionTwo = {
+  question: "Am I from the city Mumbai? ",
+  answer: "no",
+  rightAnswer: "I'm from Jodhpur!"
+}
+
+var questionThree = {
+  question: "Where do I work? ",
+  answer: "tcs",
+  rightAnswer: "Currently I work in TCS!"
+}
+
+var questionFour = {
+  question: "Do I like photography? ",
+  answer: "yes",
+  rightAnswer: "It seems you don't know me well!"
+}
+
+var questionFive = {
+  question: "Do I like Pizza? ",
+  answer: "no",
+  rightAnswer: "I don't like Pizza's... I love them!"
+}
+
+var questionList = [questionOne, questionTwo, questionThree, questionFour, questionFive]
+
+for(var i = 0; i < questionList.length; i++){
+  play(questionList[i].question, questionList[i].answer, questionList[i].rightAnswer)
+}
+
+console.log("\nYay! You Scored: " + score)
+console.log()
